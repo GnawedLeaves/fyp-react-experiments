fb1f2be6b6244503062c735dca17c6ea81fe572b
Marcel <Marcelyap31@gmail.com>
2024-01-20 18:44:52 +0800

    added conditional rendering for components

--- src/pages/Home/HomePage.js
+++ src/pages/Home/HomePage.js
@@ -1,14 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import { ThemeProvider } from "styled-components";
-import { theme } from "../../theme";
-import { HomePageContainer } from "./HomePageStyles";
+import {
+  ConditionFalseComponent,
+  ConditionTrueComponent,
+  HomePageContainer,
+} from "./HomePageStyles";
 import BlogBox from "../../components/BlogBox/BlogBox";
+import { theme } from "../../theme";
 
 const HomePage = () => {
+  const [showCondition, setShowCondition] = useState(false);
   return (
     <ThemeProvider theme={theme}>
       <HomePageContainer>Home page</HomePageContainer>
       <BlogBox title="title" />
+      <div
+        style={{
+          margin: "5rem 0",
+          border: "1px solid #333333",
+          padding: "2rem",
+        }}
+      >
+        {showCondition ? (
+          <ConditionTrueComponent>Condition is true</ConditionTrueComponent>
+        ) : (
+          <ConditionFalseComponent>Condition is false </ConditionFalseComponent>
+        )}
+        <button
+          onClick={() => {
+            setShowCondition(!showCondition);
+          }}
+        >
+          Click to toggle component
+        </button>
+      </div>
     </ThemeProvider>
   );
 };
--- src/pages/Home/HomePageStyles.js
+++ src/pages/Home/HomePageStyles.js
@@ -1,3 +1,11 @@
 import styled from "styled-components";
 
 export const HomePageContainer = styled.div``;
+
+export const ConditionFalseComponent = styled.div`
+  padding: 1rem;
+  background: salmon;
+`;
+export const ConditionTrueComponent = styled(ConditionFalseComponent)`
+  background: lime;
+`;
